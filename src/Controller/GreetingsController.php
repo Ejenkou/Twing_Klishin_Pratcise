f0dc12ffdac6b24dd6b47585c05e716b2fed4cb5
Ejenkou <ejenkou.k@mail.ru>
2023-01-15 18:53:17 +0200

    text to PHP and new twig file

--- src/Controller/GreetingsController.php
+++ src/Controller/GreetingsController.php
@@ -10,15 +10,36 @@ class GreetingsController extends AbstractController
 {
     public function index(): Response
     {
+        $labe = [
+            'poster' => 'Постер фильма*:',
+            'name' => ' Название фильма*: ',
+            'show' => '- Сейчас в кинотеатрах?',
+            'where' => 'Показ: ',
+            'zanr' => ' Жанр фильма: ',
+            'cash' => 'Кассовые сборы:',
+            'visit' => 'Количество посетивших:',
+            'rate' => 'Рейтинг:',
+            'descript' => 'Описание фильма*:'
+        ];
+
         return $this->render('introduction/index.html.twig', [
             'controller_name' => 'GreetingsController',
-            'hello_world' => 'Hello World!!'
+            'hello_world' => 'Hello World!!',
+            'labe' => $labe            
         ]);
     }
 
     public function index2(): Response
     {
         return $this->render('introduction/index2.html.twig', [
+            'controller_name' => 'GreetingsController',
+            'hello_world' => 'Hello World!!'
+        ]);
+    }
+
+    public function notif(): Response
+    {
+        return $this->render('introduction/notif.html.twig', [
             'controller_name' => 'GreetingsController',
             'hello_world' => 'Hello World!!'
         ]);
